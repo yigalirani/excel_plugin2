079bdf2e66436fedbf570664395d053aa2f1d379
Yigal Irani <yigal@symbolclick.com>
2021-12-29 15:30:25 -0500

    more simplification, got rid of the clucky polyfill and async

--- src/taskpane.js
+++ src/taskpane.js
@@ -7,28 +7,21 @@
 
 // The initialize function must be run each time a new page is loaded
 Office.initialize = () => {
-
   document.getElementById("run").onclick = run;
 };
+function mark_it(context){
+  const range = context.workbook.getSelectedRange();
 
-export async function run() {
-  try {
-    await Excel.run(async (context) => {
-      /**
-       * Insert your Excel code here
-       */
-      const range = context.workbook.getSelectedRange();
+  // Read the range address
+  range.load("address");
 
-      // Read the range address
-      range.load("address");
+  // Update the fill color
+  range.format.fill.color = "yellow";
 
-      // Update the fill color
-      range.format.fill.color = "yellow";
+  context.sync();
+  console.log(`The range address was ${range.address}.`);
+}
 
-      await context.sync();
-      console.log(`The range address was ${range.address}.`);
-    });
-  } catch (error) {
-    console.error(error);
-  }
+export function run() {
+  Excel.run(mark_it)
 }
